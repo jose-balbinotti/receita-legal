8c71ea210ec08a01e011c6e5288890abe8e533f1
Matheus Kroska <kroskamatheus@gmail.com>
2021-06-14 13:11:54 -0300

    Auth+Firestone

--- app/src/main/java/com/example/receitalegal/RegisterActivity.java
+++ app/src/main/java/com/example/receitalegal/RegisterActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,16 +13,25 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    public static final String TAG = null;
     EditText rName, rEmail, rPassword;
     Button rBtnRegister;
     TextView rBtnAlready;
     FirebaseAuth fAuth;
+    FirebaseFirestore fFirestore;
+    String uId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +45,7 @@ public class RegisterActivity extends AppCompatActivity {
         rBtnAlready = findViewById(R.id.btnAlready);
 
         fAuth = FirebaseAuth.getInstance();
+        fFirestore = FirebaseFirestore.getInstance();
 
         if(fAuth.getCurrentUser() != null){
             //fAuth.signOut();
@@ -50,6 +61,7 @@ public class RegisterActivity extends AppCompatActivity {
             public void onClick(View view) {
                 String email = rEmail.getText().toString().trim();
                 String password = rPassword.getText().toString().trim();
+                String name = rName.getText().toString().trim();
 
                 if(email.isEmpty()){
                     rEmail.setError("email required");
@@ -73,6 +85,22 @@ public class RegisterActivity extends AppCompatActivity {
                     public void onComplete(@NonNull @org.jetbrains.annotations.NotNull Task<AuthResult> task) {
                         if(task.isSuccessful()){
                             Toast.makeText(RegisterActivity.this, "User created", Toast.LENGTH_SHORT).show();
+
+                            //get user ID
+                            uId = fAuth.getCurrentUser().getUid();
+                            //creating collection users
+                            DocumentReference documentReference = fFirestore.collection("users").document(uId);
+                            Map<String,Object> user = new HashMap<>();
+                            user.put("rName",name);
+                            user.put("rEmail",email);
+                            //insert into database
+                            documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                  Log.d(TAG,"onSucess: USER PROFILE IS CREATED FOR "+ uId);
+                                }
+                            });
+
                             startActivity(new Intent(getApplicationContext(), MainActivity.class));
                         }else {
                             Toast.makeText(RegisterActivity.this, "Error !" +task.getException().getMessage(), Toast.LENGTH_SHORT).show();
