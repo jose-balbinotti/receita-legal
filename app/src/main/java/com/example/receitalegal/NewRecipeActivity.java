db6c8ceec0ef746d36b34da9c4780777db599b9e
Matheus Kroska <kroskamatheus@gmail.com>
2021-06-17 22:59:24 -0300

    RecyclerViewRecipe

--- app/src/main/java/com/example/receitalegal/NewRecipeActivity.java
+++ app/src/main/java/com/example/receitalegal/NewRecipeActivity.java
@@ -3,6 +3,7 @@ package com.example.receitalegal;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatSpinner;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
@@ -15,9 +16,11 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
@@ -36,12 +39,14 @@ import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import io.grpc.Compressor;
 
-public class NewRecipeActivity extends AppCompatActivity {
+public class NewRecipeActivity extends AppCompatActivity implements View.OnClickListener {
 
     public static final String TAG = null;
     ImageView imageView;
@@ -62,19 +67,29 @@ public class NewRecipeActivity extends AppCompatActivity {
 
     Uri imgUri;
 
+    LinearLayout layoutList;
+    Button buttonAdd;
+    Button buttonSubmitList;
 
+    List<String> teamList = new ArrayList<>();
+    ArrayList<Recipe> cricketersList = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_recipe);
 
-        imageView = findViewById(R.id.imageViewRecipe);
-        edName = findViewById(R.id.editTextRecipe);
-        edDesc = findViewById(R.id.editTextDescription);
-        edIngredient1 = findViewById(R.id.editTextIng1);
-        edIngredient2 = findViewById(R.id.editTextIng2);
-        edIngredient3 = findViewById(R.id.editTextIng3);
-        btnSaveRecipe = findViewById(R.id.btnSaveRecipe);
+        layoutList = findViewById(R.id.layout_list);
+        buttonAdd = findViewById(R.id.button_add);
+
+
+
+//        imageView = findViewById(R.id.imageViewRecipe);
+//        edName = findViewById(R.id.editTextRecipe);
+//        edDesc = findViewById(R.id.editTextDescription);
+//        edIngredient1 = findViewById(R.id.editTextIng1);
+//        edIngredient2 = findViewById(R.id.editTextIng2);
+//        edIngredient3 = findViewById(R.id.editTextIng3);
+//        btnSaveRecipe = findViewById(R.id.btnSaveRecipe);
 
         fAuth = FirebaseAuth.getInstance();
         uId = fAuth.getCurrentUser().getUid();
@@ -82,103 +97,142 @@ public class NewRecipeActivity extends AppCompatActivity {
         fFirestore = FirebaseFirestore.getInstance();
         storageReference = FirebaseStorage.getInstance().getReference();
 
+        teamList.add("Kg");
+        teamList.add("g");
+        teamList.add("Un");
+        teamList.add("ml");
+        teamList.add("l");
 
 
-
-        btnSaveRecipe.setOnClickListener(view -> {
-
-            uploadImg();
-
-            try {
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-
-            startActivity(new Intent(getApplicationContext(), RecipeActivity.class));
-            finish();
-        });
-
-
-    }
-
-    public void storeData(){
-
-        String img = imgUrl;
-        String name = edName.getText().toString().trim();
-        String description = edDesc.getText().toString().trim();
-        String ingr1 = edIngredient1.getText().toString().trim();
-        String ingr2 = edIngredient2.getText().toString().trim();
-        String ingr3 = edIngredient3.getText().toString().trim();
-
-        Map<String, Object> recipe = new HashMap<>();
-        recipe.put("img",img);
-        recipe.put("name", name);
-        recipe.put("description", description);
-        recipe.put("ingr1", ingr1);
-        recipe.put("ingr2", ingr2);
-        recipe.put("ingr3", ingr3);
-
-
-        fFirestore.collection("users").document(uId).collection("recipeBook")
-                .add(recipe)
-                .addOnSuccessListener(documentReference -> {
-                    Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
-
-                })
-                .addOnFailureListener(e -> Log.w(TAG, "Error adding document", e));
-    }
-
-    public void getDownloadLink() {
-        storageReference.child("images/"+imgUri.getLastPathSegment()).getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
-            @Override
-            public void onSuccess(Uri uri) {
-                imgUrl = uri.toString();
-                storeData();
-            }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull @NotNull Exception e) {
-                Toast.makeText(NewRecipeActivity.this, "Deu erro", Toast.LENGTH_SHORT).show();
-            }
-        });
-    }
-
-    public void uploadImg(){
-        uploadTask = storageReference.child("images/"+imgUri.getLastPathSegment()).putFile(imgUri);
-        uploadTask.addOnSuccessListener(taskSnapshot -> getDownloadLink() //Toast.makeText(this, "rá", Toast.LENGTH_SHORT).show()
-        ).addOnFailureListener(e -> Toast.makeText(NewRecipeActivity.this, "deu ruim", Toast.LENGTH_SHORT).show());
-
-    }
-
-
-    public void pickImg(View view){
-
-        if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED){
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 100);
-        } else {
-            Toast.makeText(this, "Permission Allowed", Toast.LENGTH_SHORT).show();
-        }
-
-
-        Intent intent = new Intent(Intent.ACTION_PICK);
-        intent.setType("image/*");
-        startActivityForResult(intent,1);
+        buttonAdd.setOnClickListener(this);
+//        btnSaveRecipe.setOnClickListener(view -> {
+//
+//            uploadImg();
+//
+//            try {
+//                Thread.sleep(2000);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//
+//            startActivity(new Intent(getApplicationContext(), RecipeActivity.class));
+//            finish();
+//        });
 
 
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable @org.jetbrains.annotations.Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if(resultCode ==RESULT_OK){
-            imgUri=data.getData();
-            imageView.setImageURI(imgUri);
-        }else {
-            Toast.makeText(this, "you have not picked img", Toast.LENGTH_SHORT).show();
-        }
+    public void onClick(View v) {
+      addView();
     }
+
+
+
+    private void addView(){
+
+        final View cricketerView = getLayoutInflater().inflate(R.layout.row_add_recipe,null,false);
+
+        EditText editText = (EditText)cricketerView.findViewById(R.id.edit_cricketer_name);
+        AppCompatSpinner spinnerTeam = (AppCompatSpinner)cricketerView.findViewById(R.id.spinner_team);
+        ImageView imageClose = (ImageView)cricketerView.findViewById(R.id.image_remove);
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this,android.R.layout.simple_spinner_item,teamList);
+        spinnerTeam.setAdapter(arrayAdapter);
+
+
+        imageClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                removeView(cricketerView);
+            }
+        });
+
+        layoutList.addView(cricketerView);
+    }
+
+    private void removeView(View view){
+        layoutList.removeView(view);
+    }
+
+
+//    public void storeData(){
+//
+//        String img = imgUrl;
+//        String name = edName.getText().toString().trim();
+//        String description = edDesc.getText().toString().trim();
+//        String ingr1 = edIngredient1.getText().toString().trim();
+//        String ingr2 = edIngredient2.getText().toString().trim();
+//        String ingr3 = edIngredient3.getText().toString().trim();
+//
+//        Map<String, Object> recipe = new HashMap<>();
+//        recipe.put("img",img);
+//        recipe.put("name", name);
+//        recipe.put("description", description);
+//        recipe.put("ingr1", ingr1);
+//        recipe.put("ingr2", ingr2);
+//        recipe.put("ingr3", ingr3);
+//
+//
+//        fFirestore.collection("users").document(uId).collection("recipeBook")
+//                .add(recipe)
+//                .addOnSuccessListener(documentReference -> {
+//                    Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+//
+//                })
+//                .addOnFailureListener(e -> Log.w(TAG, "Error adding document", e));
+//    }
+//
+//    public void getDownloadLink() {
+//        storageReference.child("images/"+imgUri.getLastPathSegment()).getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+//            @Override
+//            public void onSuccess(Uri uri) {
+//                imgUrl = uri.toString();
+//                storeData();
+//            }
+//        }).addOnFailureListener(new OnFailureListener() {
+//            @Override
+//            public void onFailure(@NonNull @NotNull Exception e) {
+//                Toast.makeText(NewRecipeActivity.this, "Deu erro", Toast.LENGTH_SHORT).show();
+//            }
+//        });
+//    }
+//
+//    public void uploadImg(){
+//        uploadTask = storageReference.child("images/"+imgUri.getLastPathSegment()).putFile(imgUri);
+//        uploadTask.addOnSuccessListener(taskSnapshot -> getDownloadLink() //Toast.makeText(this, "rá", Toast.LENGTH_SHORT).show()
+//        ).addOnFailureListener(e -> Toast.makeText(NewRecipeActivity.this, "deu ruim", Toast.LENGTH_SHORT).show());
+//
+//    }
+//
+//
+//    public void pickImg(View view){
+//
+//        if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED){
+//            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 100);
+//        } else {
+//            Toast.makeText(this, "Permission Allowed", Toast.LENGTH_SHORT).show();
+//        }
+//
+//
+//        Intent intent = new Intent(Intent.ACTION_PICK);
+//        intent.setType("image/*");
+//        startActivityForResult(intent,1);
+//
+//
+//    }
+//
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, @Nullable @org.jetbrains.annotations.Nullable Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//
+//        if(resultCode ==RESULT_OK){
+//            imgUri=data.getData();
+//            imageView.setImageURI(imgUri);
+//        }else {
+//            Toast.makeText(this, "you have not picked img", Toast.LENGTH_SHORT).show();
+//        }
+//    }
 
 
 
